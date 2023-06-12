bb2fa21246e39c1d2370afcf844daf66f3ee8cb3
REDfriend <tongtongkonthai.34930@gmail.com>
2023-06-12 19:18:41 +0700

    first commmit

--- src/KMITL.jsx
+++ src/KMITL.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 
 function KMITL() {
   return (
-    <div>
-        <div className=' w-96 h-auto text-center'>
-            <h1>Dark-Side</h1>
-        </div>
-    </div>
+    <>
+    
+      <div className=' ease-in-out duration-700 w-96 h-fit outline outline-orange-800 hover:scale-105 hover:outline-orange-300 scale-125'>
+          <div className='ease-in-out duration-700'>
+            <img className='ease-in-out duration-700' src="src\assets\Artboard 1.png" alt="" />
+          </div>
+
+      </div>
+    </>
   )
 }
 
