29c512b6c6c99ec17be7b14c11b7274b0ccc1811
REDfriend <83909061+1REDfriend@users.noreply.github.com>
2023-09-14 21:31:44 +0700

    add image and fix button

--- src/KMITL.jsx
+++ src/KMITL.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
-import { Route } from 'react-router-dom'
+import React from 'react';
+import { useNavigate  } from 'react-router-dom';
 
 function KMITL() {
+  const navigate  = useNavigate ();
+
   return (
     <>
-    
-      <div className=' ease-in-out duration-700 w-96 h-fit outline outline-orange-800 hover:scale-105 hover:outline-orange-300 scale-125'>
-          <div className='ease-in-out duration-700'>
-            <img className='ease-in-out duration-700' src="src\assets\Artboard 1.png" alt="" />
-          </div>
-
+      <div className='text-left ease-in-out duration-700 mt-3'>
+        <button className='bg-orange-500 hover:bg-white hover:text-black' onClick={() => { navigate(-1); }}>
+          BACK
+        </button>
+      </div>
+      <div className='flex ease-in-out duration-700 w-auto h-fit outline outline-orange-800 scale-100 hover:scale-95 hover:outline-orange-300 m-10'>
+        <div className='ease-in-out duration-700 pr-3'>
+          <img className='ease-in-out duration-700' src="src\assets\1.png" alt="" />
+        </div>
+        <div className='ease-in-out duration-700'>
+          <img className='ease-in-out duration-700' src="src\assets\2.png" alt="" />
+        </div>
       </div>
     </>
   )
 }
 
-export default KMITL
+export default KMITL;
